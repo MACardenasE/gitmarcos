4dd3b34a1625f955fc5dcd61195d110ef3643cb3
MACardenasE <aeae.mcmc@gmail.com>
2025-02-19 14:09:34 -0600

    Delete ejercicio13.py

--- ejercicio13.py
+++
@@ -1,4 +0,0 @@
-#usar una variable para  almacenar una oracion y modificarla con +=
-frase = "Juventud, !Divino Tesoro!"
-frase_modificada = frase + ", ya te vas para no volver."
-print(frase_modificada)
